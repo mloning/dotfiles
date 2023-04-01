6da2c4c7888004255e97075bee21b62239d7aa1a
mloning <21020482+mloning@users.noreply.github.com>
2023-04-01 15:50:41 +0100

    add fzf extension to telescope

--- nvim/.config/nvim/lua/user/telescope.lua
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -31,4 +31,16 @@ telescope.setup({
             },
         },
     },
+    extensions = {
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case",
+        },
+    },
 })
+
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function
+require("telescope").load_extension("fzf")

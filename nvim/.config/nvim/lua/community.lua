f2e9b89e3733303c10527847f300882e5067b72b
mloning <21020482+mloning@users.noreply.github.com>
2024-09-12 09:29:14 +0200

    add copilot cmp, update config

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -10,5 +10,6 @@ return {
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.completion.copilot-cmp" },
   -- import/override with your plugins folder
 }
--- nvim/.config/nvim/lua/plugins/overridden.lua
+++ nvim/.config/nvim/lua/plugins/overridden.lua
@@ -9,4 +9,17 @@ return {
       },
     },
   },
+  {
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+      -- customize lsp formatting options
+      formatting = {
+        -- control auto formatting on save
+        format_on_save = {
+          enabled = false, -- enable or disable format on save globally
+        },
+      },
+    },
+  },
 }

08e5e48133ea9e16037bc3e339c0c3867fd98ff9
mloning <21020482+mloning@users.noreply.github.com>
2024-09-18 14:24:49 +0200

    enable format on save again

--- nvim/.config/nvim/lua/plugins/overridden.lua
+++ nvim/.config/nvim/lua/plugins/overridden.lua
@@ -9,17 +9,4 @@ return {
       },
     },
   },
-  {
-    "AstroNvim/astrolsp",
-    ---@type AstroLSPOpts
-    opts = {
-      -- customize lsp formatting options
-      formatting = {
-        -- control auto formatting on save
-        format_on_save = {
-          enabled = false, -- enable or disable format on save globally
-        },
-      },
-    },
-  },
 }

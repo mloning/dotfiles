857735b7777ce55e5062aed8228c24c807201475
mloning <21020482+mloning@users.noreply.github.com>
2026-03-03 10:47:17 +0100

    nvim: replace built-in qfixlist with trouble

--- nvim/.config/nvim/lua/plugins/overridden.lua
+++ nvim/.config/nvim/lua/plugins/overridden.lua
@@ -15,6 +15,27 @@ return {
           ["N"] = { cmd = "Nzz", desc = "Previous search result with centering" },
         },
       },
+      autocmds = {
+        -- Redirect quickfix/location list to Trouble
+        trouble_qf = {
+          {
+            event = "FileType",
+            pattern = "qf",
+            desc = "Redirect quickfix and location lists to Trouble",
+            callback = function()
+              local is_loclist = vim.fn.getloclist(0, { filewinid = 1 }).filewinid ~= 0
+              vim.schedule(function()
+                vim.cmd("close")
+                if is_loclist then
+                  require("trouble").open("loclist")
+                else
+                  require("trouble").open("qflist")
+                end
+              end)
+            end,
+          },
+        },
+      },
     },
   },
 }

cff86a0d35bb4efde17c13cfe15668501d9265e6
mloning <21020482+mloning@users.noreply.github.com>
2026-09-16 11:55:02 +0200

    docker update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,7 @@
+# The following lines were added by Docker Desktop to add commands to your PATH.
+export PATH="$PATH:/Users/mloning/.docker/bin"
+# End of Docker Desktop section.
+
 if status is-interactive
     # Remove fish greeting
     set -g fish_greeting
